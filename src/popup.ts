0f840bf6c8096d45b933ff11322d64974efb2083
Dragantic <dwagantic@gmail.com>
2023-02-11 03:04:08 -0500

    use single quotes

--- src/popup.ts
+++ src/popup.ts
@@ -1,27 +1,27 @@
 let manifest = browser.runtime.getManifest();
-let header = document.getElementsByClassName("ext-version");
+let header = document.getElementsByClassName('ext-version');
 if (header.length > 0) {
 	let text = document.createTextNode(manifest.version);
 	header[0].appendChild(text);
 }
 
-document.addEventListener("click", (e) => {
+document.addEventListener('click', (e) => {
 	if (e.target) {
 		let target = <HTMLElement>e.target;
 		switch (target.id) {
-		case "img-download-left":
+		case 'img-download-left':
 			browser.tabs.query({ active: true, currentWindow: true })
 			.then( (active) => {
 				browser.runtime.sendMessage({left: active[0].index});
 			});
 			break;
-		case "img-download-right":
+		case 'img-download-right':
 			browser.tabs.query({ active: true, currentWindow: true })
 			.then( (active) => {
 				browser.runtime.sendMessage({right: active[0].index});
 			});
 			break;
-		case "cancel-download":
+		case 'cancel-download':
 			browser.runtime.sendMessage({cancel: true});
 			break;
 		default:
