158157798d1adeb8c2450e077cfe040577f76567
Dragantic <dwagantic@gmail.com>
2023-02-16 08:42:18 -0500

    remove the unnecessary ready clause

--- src/content.ts
+++ src/content.ts
@@ -1,7 +1,6 @@
-let btn = document.createElement('div')
+const btn = document.createElement('div')
 btn.id = 'fatabs-button'
 btn.title = 'Download Image'
-btn.style.visibility = 'hidden'
 
 let src: string
 let link: string
@@ -17,18 +16,15 @@ document.addEventListener('mouseover', (e) => {
 		let a = target.closest('a')
 		link = a ? a.href : target.baseURI
 		src = target.src
-
 		target.before(btn)
-		btn.style.visibility = 'visible'
 	} else if (target !== btn) {
-		btn.style.visibility = 'hidden'
+		btn.remove()
 	}
 })
 
-
-let ready = () => document.body.appendChild(btn)
-if (document.readyState !== 'loading') {
-	ready()
-} else {
+let ready = () => document.dispatchEvent(new Event('mouseover'))
+if (document.readyState === 'loading') {
 	document.addEventListener('DOMContentLoaded', ready)
+} else {
+	ready()
 }
