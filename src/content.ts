f38ad77de594239ec23dcec69e9c47cd71ac103d
Dragantic <dwagantic@gmail.com>
2023-02-26 11:08:40 -0500

    don't reposition button if target is already the image

--- src/content.ts
+++ src/content.ts
@@ -1,15 +1,21 @@
-let img: HTMLImageElement | HTMLVideoElement
+let img: HTMLImageElement | HTMLVideoElement | null
 
 const btn = document.createElement('div')
-btn.id = 'fatabs-button'
+btn.id = 'fatabs'
 btn.title = 'Download Image'
 btn.addEventListener('click', (e) => {
+	if (!img) {
+		return
+	}
 	let src: string
 	let filename: string | undefined
 
-	if (img instanceof HTMLVideoElement) {
-		let vid: HTMLSourceElement | null = img.querySelector('source')
-		src = vid ? vid.src : img.src
+	if (!img.src) {
+		const vid = img.querySelector('source')
+		if (!vid) {
+			return
+		}
+		src = vid.src
 	} else {
 		src = img.src
 	}
@@ -22,14 +28,13 @@ btn.addEventListener('click', (e) => {
 		filename = (artist ? artist[1].replace(/[_ ]/g, '-') + '_' : '') + derp[1]
 		         + src.substring(src.lastIndexOf('.'))
 	} else {
-		const a = img.closest('a')
-		const link = a ? a.href : img.baseURI
+		const link = img.closest('a')?.href || img.baseURI
 		const twitter = link.match(/.*?:\/\/twitter\.com\/([a-zA-Z0-9_]+)/)
 		if (twitter) {
-			const end = src.indexOf('?')
-			const s = src.substring(0, end > 0 ? end : undefined)
-			let ext = new URLSearchParams(src.substring(end)).get('format')
-			ext = ext ? ext : 'jpg'
+			const q = src.indexOf('?')
+			const s = src.substring(0, (q > 0) ? q : undefined)
+			const ext = (q > 0) &&
+				new URLSearchParams(src.substring(q)).get('format') || 'jpg'
 			filename = twitter[1].replace(/_/g, '-') + '_'
 			         + s.substring(s.lastIndexOf('/') + 1) + '.' + ext
 			src = s + '?format=' + ext + '&name=orig'
@@ -43,13 +48,16 @@ btn.addEventListener('click', (e) => {
 })
 
 document.addEventListener('mouseover', (e) => {
+	if (e.target === img) {
+		return
+	}
 	if ((e.target instanceof HTMLImageElement || e.target instanceof HTMLVideoElement)
-	 && (e.target.offsetWidth >= 260 && e.target.offsetHeight >= 260)) {
+	 && (e.target.offsetWidth >= 240 && e.target.offsetHeight >= 240)) {
 		img = e.target
-		const neighbor = (img instanceof HTMLVideoElement && img.parentElement) ?
-			img.parentElement : img
+		const neighbor = img instanceof HTMLVideoElement && img.parentElement || img
 		neighbor.before(btn)
 	} else if (e.target !== btn) {
+		img = null
 		btn.remove()
 	}
 })
